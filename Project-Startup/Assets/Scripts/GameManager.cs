7c0426687ed5d3a8c6ee734f9bd20484b57c8ff9
danielaiv <voicuioanadaniela@yahoo.com>
2025-01-19 21:02:10 +0100

    yay

--- Project-Startup/Assets/Scripts/GameManager.cs
+++ Project-Startup/Assets/Scripts/GameManager.cs
@@ -1,4 +1,7 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
@@ -7,15 +10,25 @@ public class GameManager : MonoBehaviour
     public GameObject curSceneWorld;
 
     public bool userFrozen = false;
+    public bool interactingObj = false; // set this to true with object interactions so the camera doesn't move
+    public bool interactingUI; // no need to update this one
+    public bool canMove = true;
+
+    public List<Fella> fellaDex; // all collected fellas not in home or concert
+    public List<Fella> fellasInHomeRoom; // spawn fellas here when captured
+    public List<Fella> fellasInConcertRoom;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if(GameManager.instance == null)
+        if (GameManager.instance == null)
         {
             instance = this;
-        }    
+            fellaDex = new List<Fella>();
+            fellasInHomeRoom = new List<Fella>();
+            fellasInConcertRoom = new List<Fella>();
+        }
         else
         {
             Destroy(this);
@@ -25,6 +38,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        interactingUI = EventSystem.current.IsPointerOverGameObject();
     }
 }
---
+++ Project-Startup/Assets/Scripts/PanZoomRotate.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+public class PanZoomRotate : MonoBehaviour
+{
+    public float fovMin = 20f;
+    public float fovMax = 60f;
+    public float panSpeed = 0.5f;
+    public float zoomSpeed = 10f;
+    public float rotationSpeed = 100f;
+
+    public Vector2 pitchLimits = new Vector2(10f, 80f);
+
+    Vector3 touchStart;
+    float yaw = 0f;
+    float pitch;
+    private void Awake()
+    {
+        yaw = Camera.main.transform.eulerAngles.y;
+        pitch = Camera.main.transform.eulerAngles.x;
+    }
+    void Update()
+    {
+        if (GameManager.instance.interactingUI || GameManager.instance.interactingObj || !GameManager.instance.canMove) return;
+        
+        if (Input.GetMouseButtonDown(0))
+            touchStart = getWorldPoint(Input.mousePosition);
+        else if (Input.GetMouseButtonDown(1))
+        {
+            yaw = Camera.main.transform.eulerAngles.y;
+            pitch = Camera.main.transform.eulerAngles.x;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 direction = touchStart - getWorldPoint(Input.mousePosition);
+            direction.y = 0;
+
+            Camera.main.transform.position += direction * panSpeed;
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            yaw += mouseX * rotationSpeed * Time.deltaTime;
+
+            pitch -= mouseY * rotationSpeed * Time.deltaTime;
+            pitch = Mathf.Clamp(pitch, pitchLimits.x, pitchLimits.y);
+
+            Camera.main.transform.eulerAngles = new Vector3(pitch, yaw, 0);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (Mathf.Abs(scroll) > 0.01f)
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView - scroll * zoomSpeed, fovMin, fovMax);
+
+    }
+    Vector3 getWorldPoint(Vector3 screenPos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero); // could change this with collision layers and adjust cam height if needed
+        if (groundPlane.Raycast(ray, out float hitDistance))
+        {
+            return ray.GetPoint(hitDistance);
+        }
+        return Vector3.zero;
+    }
+}
